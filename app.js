34d102254b573b1dc19b6008588c24bec399efd0
unknown <andresfelipehc@ufps.edu.co>
2022-05-27 16:51:52 -0500

    Modificacion previo

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const hbs = require('hbs');
 
 // Inicializando el server
 const app = express();
 
+
 // Configurando el motor de plantillas HANDLEBARS
 app.set('view engine', 'hbs');
-hbs.registerPartials(__dirname + '/public/partials');
-
+hbs.registerPartials(__dirname + '/views/partials');
 
 //Middlewares
+app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('views'));
+
 
 // Creando rutas
 const ruta = {
@@ -20,6 +23,6 @@ const ruta = {
 app.use(ruta.app, require('./routes/control'));
 
 // Corriendo el server
-app.listen(4000, ()=>{
-    console.log('App en el puerto 4000')
+app.listen(3000, ()=>{
+    console.log('App en el puerto 3000')
 })
--- routes/control.js
+++ routes/control.js
@@ -1,21 +1,47 @@
 const { Router } = require('express');
-
 const router = Router();
 const axios = require('axios');
+const req = require('express/lib/request');
+const async = require('hbs/lib/async');
 
-router.get('/prueba', async(req, res )=>{
+
+router.get('/consultarCantidades', (req, res) => {
+    res.render('consultarCantidades');
+})
+
+router.post('/consultarCantidades', async (req, res) => {
+
+    const { grupo } = req.body;
 
     const instance = axios.create({
-        baseURL: 'http://localhost:3000/api/pruebas'
+        baseURL: `http://localhost:4000/prod/productos/${grupo}`
     })
 
     const result = await instance.get();
 
-    const data = JSON.stringify(result.data.msg);
+    const data = JSON.stringify(result.data.cont);
 
-    res.render('../public/index', {
+    res.render('../views/consultarCantidades', {
         data
     });
 });
+
+router.get('/registrarProducto', async(req, res) => {
+
+    const instance = axios.create({
+        baseURL: `http://localhost:4000/prod/productos`
+    })
+
+    const result = await instance.get();
+
+    const productos = JSON.stringify(result.data.productos);
+
+    console.log(productos)
+
+    res.render('registrarProducto', [productos]);
+
+})
+
+
 
 module.exports = router;
