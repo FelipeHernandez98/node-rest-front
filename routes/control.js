1aafc9518b98a638495bf1f329c4d9a7d8330686
FelipeHernandez98 <andresfelipehc@ufps.edu.co>
2022-05-28 00:43:25 -0500

    Creando nuevas rutas

--- routes/control.js
+++ routes/control.js
@@ -1,8 +1,6 @@
 const { Router } = require('express');
 const router = Router();
 const axios = require('axios');
-const req = require('express/lib/request');
-const async = require('hbs/lib/async');
 
 
 router.get('/consultarCantidades', (req, res) => {
@@ -26,21 +24,69 @@ router.post('/consultarCantidades', async (req, res) => {
     });
 });
 
-router.get('/registrarProducto', async(req, res) => {
+router.get('/registrarProducto', async (req, res) => {
 
     const instance = axios.create({
         baseURL: `http://localhost:4000/prod/productos`
     })
 
-    const result = await instance.get();
+    let productos = await instance.get();
 
-    const productos = JSON.stringify(result.data.productos);
+    productos = productos.data;
 
-    console.log(productos)
 
-    res.render('registrarProducto', [productos]);
+
+    res.render('registrarProducto', { productos });
 
 })
+
+router.post('/registrarProducto', async (req, res) => {
+
+    const { nombre, cantidad, subcategoria, fecha, grupo, programa, facultad } = req.body;
+
+    axios({
+        method: 'POST',
+        url: `http://localhost:4000/prod/addProd`,
+        data: {
+            nombre,
+            cantidad,
+            subcategoria,
+            fecha,
+            grupo,
+            programa,
+            facultad
+        }
+    })
+    .then((respuesta)=>{
+        res.redirect('registrarProducto')
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+
+    
+})
+
+router.get('/consultarCantTipo', (req, res)=>{
+    res.render('consultarCantTipo');
+})
+
+router.post('/consultarCantTipo', async (req, res) => {
+
+    const { tipo } = req.body;
+
+    const instance = axios.create({
+        baseURL: `http://localhost:4000/prod/cantidad/${tipo}`
+    })
+
+    const result = await instance.get();
+
+    const data = JSON.stringify(result.data.cant);
+
+    res.render('../views/consultarCantTipo', {
+        data
+    });
+});
 
 
 
